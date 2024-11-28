f4602f0b1065531e890ed11e96246a52687b86ae
jianqun.wang <jianqun.wang@amlogic.com>
2024-11-28 17:49:32 +0800

    Optimize Generate Patch tool

--- Generate_Patch.sh
+++ Generate_Patch.sh
@@ -17,41 +17,43 @@ git_root_path=$(git rev-parse --show-toplevel)
 
 issue_path="$1"
 
-# 检查当前目录是否在 repo 根目录下
-if [[ $git_root_path == $s_repo_root_path* ]]; then
-  # 获取当前 Git 仓库相对于 repo 根目录的相对路径
-  relative_path=${git_root_path#$s_repo_root_path/}
-
-  # 创建输出目录，包括相对路径
-  output_dir="$patch_path/$issue_path/$relative_path"
-  mkdir -p "$output_dir"
-
-  # 生成 patch 文件
-  git format-patch -1 HEAD -o $output_dir
-  echo "Patch for $s_repo_root_path has been generated in $output_dir"
-
-elif [[ $git_root_path == $s_gretzky_root_path* ]]; then
-  # 获取当前 Git 仓库相对于 s_gretzky_root_path 根目录的相对路径
-  relative_path=${git_root_path#$s_gretzky_root_path/}
-
-  # 创建输出目录，包括相对路径
-  output_dir="$s_gretzky_patch_path/$issue_path/$relative_path"
-  mkdir -p "$output_dir"
-
-  # 生成 patch 文件
-  git format-patch -1 HEAD -o "$output_dir"
-  echo "Patch for $s_gretzky_root_path has been generated in $output_dir"
-elif [[ $git_root_path == $u_ab2_root_path* ]]; then
-  # 获取当前 Git 仓库相对于 s_gretzky_root_path 根目录的相对路径
-  relative_path=${git_root_path#$u_ab2_root_path/}
-
-  # 创建输出目录，包括相对路径
-  output_dir="$u_ab2_patch_path/$issue_path/$relative_path"
-  mkdir -p "$output_dir"
-
-  # 生成 patch 文件
-  git format-patch -1 HEAD -o "$output_dir"
-  echo "Patch for $u_ab2_root_path has been generated in $output_dir"
+# 根据账号定义对应的zte-project-patch 目录
+HOSTNAME=`hostname`
+USERNAME=`whoami`
+if [ "$HOSTNAME" == "droid16-sz" ] && [ "$USERNAME" == "jianqun.wang" ]; then
+    zte_project_patch="/mnt/fileroot2/jianqun.wang/zte-project-patch"
+elif [ "$HOSTNAME" == "droid13-sz" ] && [ "$USERNAME" == "jianqun.wang" ]; then
+    zte_project_patch="/mnt/nfsroot/jianqun.wang/zte-project-patch"
 else
-  echo "当前 Git 仓库不位于指定的 repo 根目录下。"
+    echo "no zte-project-patch work dir of jianqun.wang"
+fi
+
+# 定义函数来生成patch
+generate_patch() {
+    local root_path=$1
+    local patch_dir=$2
+    local issue_path=$3
+    local relative_path=${git_root_path#$root_path/}
+    local output_dir="$patch_dir/$issue_path/$relative_path"
+
+    mkdir -p "$output_dir"
+    git format-patch -1 HEAD -o "$output_dir"
+    echo "Patch for $root_path has been generated in $output_dir"
+}
+
+
+# 更新响应的zte-project-patch 目录
+cd $zte_project_patch
+git pull
+cd - 
+
+# 生成patch
+if [[ $git_root_path == $s_repo_root_path* ]]; then
+    generate_patch "$s_repo_root_path" "$patch_path" "$issue_path"
+elif [[ $git_root_path == $s_gretzky_root_path* ]]; then
+    generate_patch "$s_gretzky_root_path" "$s_gretzky_patch_path" "$issue_path"
+elif [[ $git_root_path == $u_ab2_root_path* ]]; then
+    generate_patch "$u_ab2_root_path" "$u_ab2_patch_path" "$issue_path"
+else
+    echo "当前 Git 仓库不位于指定的 repo 根目录下。"
 fi
